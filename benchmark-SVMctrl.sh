0ff4962a1bd5d8cf1d7a014ef1894ab5b250a53f
Yiding Chang <colossuschang@sjtu.edu.cn>
2023-05-04 20:32:31 -0400

    update svm bm script to increase batches

--- benchmark-SVMctrl.sh
+++ benchmark-SVMctrl.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
 
-sed -i 's/int max_iter = max(5000/int max_iter = max(100/g' src/svm.cpp
-make
+for i in {1..5}
+do
+  sed -i 's/int max_iter = max(5000/int max_iter = max(100/g' src/svm.cpp
+  make
 
-bash benchmark-fixed.sh ghc-100.csv
+  bash benchmark-fixed.sh ghc-100-${i}.csv
 
-sed -i 's/int max_iter = max(100/int max_iter = max(500/g' src/svm.cpp
-make
+  sed -i 's/int max_iter = max(100/int max_iter = max(500/g' src/svm.cpp
+  make
 
-bash benchmark-fixed.sh ghc-500.csv
+  bash benchmark-fixed.sh ghc-500-${i}.csv
 
-sed -i 's/int max_iter = max(500/int max_iter = max(1000/g' src/svm.cpp
-make
+  sed -i 's/int max_iter = max(500/int max_iter = max(1000/g' src/svm.cpp
+  make
 
-bash benchmark-fixed.sh ghc-1000.csv
+  bash benchmark-fixed.sh ghc-1000-${i}.csv
 
-sed -i 's/int max_iter = max(1000/int max_iter = max(2000/g' src/svm.cpp
-make
+  sed -i 's/int max_iter = max(1000/int max_iter = max(2000/g' src/svm.cpp
+  make
 
-bash benchmark-fixed.sh ghc-2000.csv
+  bash benchmark-fixed.sh ghc-2000-${i}.csv
 
-sed -i 's/int max_iter = max(2000/int max_iter = max(5000/g' src/svm.cpp
-make
+  sed -i 's/int max_iter = max(2000/int max_iter = max(5000/g' src/svm.cpp
+  make
+done
